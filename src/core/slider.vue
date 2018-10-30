54514918fc642b030d41ccedd23980c3525b71ab
liuzhiqiang <3436833404@qq.com>
2018-10-30 23:04:14 +0800

    Initial commit

---
+++ src/core/slider.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+          <swiper-slide>aaaa</swiper-slide>
+        </swiper>
+    </section>
+</template>
+
+<script>
+  import { swiper, swiperSlide } from "vue-awesome-swiper"
+  export default {
+        name: "slider",
+        components:{
+          swiper,
+          swiperSlide,
+        },
+        props:{
+          options:{
+            type:Object,
+            default(){
+              return{
+                autoplay:true,
+                loop:true,
+                pagination:{
+                  el:".swiper-pagination",
+                },
+                notNextTick:false,
+              }
+            }
+          }
+        }
+    }
+</script>
+
+<style lang="scss">
+  @import "~swiper/dist/css/swiper.css";
+</style>
---
+++ src/home/borrow.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <Panel title="极速借贷" :class="$style.panel">
+    <section :class="$style.content">
+      <router-link :to="{name:'home',params:{}}">
+        <img src="//img12.360buyimg.com/jrpmobile/jfs/t25759/60/2272607322/48838/438e6aad/5bc6f6a5Ncd00b8cb.jpg?width=750&height=320" alt="">
+      </router-link>
+    </section>
+  </Panel>
+</template>
+
+<script>
+    import Panel from "../core/panel";
+    export default {
+        name: "borrow",
+        components: {Panel}
+    }
+</script>
+
+<style lang="scss" module>
+  @import "../assets/css/element.scss";
+  .panel{
+    @include panel;
+    .content{
+      padding-bottom: 40px;
+      img{
+        display: block;
+        width: 100%;
+      }
+    }
+  }
+</style>
